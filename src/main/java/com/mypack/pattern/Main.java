10b4cf9ca5f4c66bda4482c9bc0a41ac40cf2f94
JiangTaoHou <925147954@qq.com>
2021-09-02 00:31:20 +0800

    替换连续的数字

--- src/main/java/com/mypack/pattern/Main.java
+++ src/main/java/com/mypack/pattern/Main.java
@@ -5,6 +5,13 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        String s1 = "ab12c3d456efg7h89i1011jk12lmn";
+        //连续的数字
+        String s2 = s1.replaceAll("\\d+","**");
+        //连续的两个数字
+        String s3 = s1.replaceAll("\\d{2}","**");
+        System.out.println(s2);
+        System.out.println(s3);
     }
 
     private static void replaceWords() {
