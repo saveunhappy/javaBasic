ab2cd9d780e2ff33f1d2775a524e5ae3585d5fd4
JiangTaoHou <925147954@qq.com>
2021-09-14 23:42:34 +0800

    逐个读取字符

--- src/main/java/com/mypack/file/io/Main.java
+++ src/main/java/com/mypack/file/io/Main.java
@@ -2,17 +2,21 @@ package com.mypack.file.io;
 
 import com.mypack.file.Files.Files;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 //        Files.write("231234哈哈哈".getBytes(),new File("E:/a/b/1.txt"));
 //        byte[] bytes = Files.read(new File("E:/a/b/1.txt"));
 //        System.out.println(new String(bytes));
-        Files.copy(new File("E:/a/b/1.txt"),new File("E:/ab/ab/30.txt"));
+//        Files.copy(new File("E:/a/b/1.txt"),new File("E:/ab/ab/30.txt"));
+        try (FileReader reader = new FileReader(new File("E:/a/b/s.txt"))) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char)c);
+                Thread.sleep(10);
+            }
+        }
     }
 
     private static void testFileInput() throws IOException {
@@ -20,13 +24,13 @@ public class Main {
         FileInputStream fis = new FileInputStream(file);
         byte[] bytes = new byte[1024];
         int len = fis.read(bytes);
-        String s = new String(bytes,0,len,"GBK");
-        System.out.println(s+"_");
+        String s = new String(bytes, 0, len, "GBK");
+        System.out.println(s + "_");
         fis.close();
     }
 
-    static void testFos() throws Exception{
-        FileOutputStream fos = new FileOutputStream("E:/1.txt",true);
+    static void testFos() throws Exception {
+        FileOutputStream fos = new FileOutputStream("E:/1.txt", true);
         fos.write("MJ码哥".getBytes("GBK"));
         fos.close();
     }
