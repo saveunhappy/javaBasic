7cdae1d01b44cafa9e209bbc622c862c034686ec
JiangTaoHou <925147954@qq.com>
2021-09-14 19:05:30 +0800

    删除和读取通用方法

--- src/main/java/com/mypack/file/Files/Files.java
+++ src/main/java/com/mypack/file/Files/Files.java
@@ -1,9 +1,29 @@
 package com.mypack.file.Files;
 
-import java.io.File;
+import java.io.*;
 import java.util.function.Consumer;
 
 public class Files {
+    public static void delete(File file) {
+        if (file == null || !file.exists()) return;
+        //这个清除的是目录，如果到里面发现是文件，直接就return了，clean中也是一直在调用delete在删除文件
+        //删除目录里面的文件成功之后，再删除自己，就是文件夹。
+        clean(file);
+        file.delete();
+    }
+
+    private static void clean(File dir) {
+        if (dir == null || !dir.exists()) return;
+        if (dir.isFile()) return;
+        File[] files = dir.listFiles();
+        assert files != null;
+        for (File sf : files) {
+            delete(sf);
+        }
+    }
+    public static void move(String src, String dest) {
+        move(new File(src),new File(dest));
+    }
 
     public static void move(File src, File dest) {
         if (src == null || dest == null) return;
@@ -15,7 +35,7 @@ public class Files {
 
     private static void mkparents(File dest) {
         File parent = dest.getParentFile();
-        if(parent.exists())return;
+        if (parent.exists()) return;
         parent.mkdirs();
     }
 
@@ -30,10 +50,35 @@ public class Files {
         }
     }
 
+    public static void write(byte[] bytes, File file) {
+        if (bytes == null || file == null) return;
+        if (file.exists()) return;
+        mkparents(file);
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] read(File file){
+        if(file == null || !file.exists())return null;
+        if(file.isDirectory())return null;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] bytes = new byte[(int)file.length()];
+            fis.read(bytes);
+            return bytes;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
 //        search(new File("E:\\datastructure"), file -> {
 //            System.out.println(file);
 //        });
-        move(new File("E:\\copy\\ccc.txt"),new File("E:\\a\\b\\ccc.txt"));
+//        move(new File("E:\\copy\\ccc.txt"), new File("E:\\a\\b\\ccc.txt"));
+
     }
 }
---
+++ src/main/java/com/mypack/file/io/Main.java
@@ -0,0 +1,32 @@
+package com.mypack.file.io;
+
+import com.mypack.file.Files.Files;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+//        Files.write("231234哈哈哈".getBytes(),new File("E:/a/b/1.txt"));
+        byte[] bytes = Files.read(new File("E:/a/b/1.txt"));
+        System.out.println(new String(bytes));
+    }
+
+    private static void testFileInput() throws IOException {
+        File file = new File("E:/1.txt");
+        FileInputStream fis = new FileInputStream(file);
+        byte[] bytes = new byte[1024];
+        int len = fis.read(bytes);
+        String s = new String(bytes,0,len,"GBK");
+        System.out.println(s+"_");
+        fis.close();
+    }
+
+    static void testFos() throws Exception{
+        FileOutputStream fos = new FileOutputStream("E:/1.txt",true);
+        fos.write("MJ码哥".getBytes("GBK"));
+        fos.close();
+    }
+}
