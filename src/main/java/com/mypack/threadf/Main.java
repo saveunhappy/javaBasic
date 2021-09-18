77fec97aa5dc3f9649a276e298c8fb6270711826
JiangTaoHou <925147954@qq.com>
2021-09-18 22:48:28 +0800

    sleep,interrupt

--- src/main/java/com/mypack/threadf/Main.java
+++ src/main/java/com/mypack/threadf/Main.java
@@ -2,7 +2,27 @@ package com.mypack.threadf;
 
 public class Main {
     public static void main(String[] args) {
-        ThreadState();
+        /**
+         * 不要想着顺序执行，1 2 3 了，执行了打印1，睡了，然后新的线程启动了
+         * 睡了1秒，打印了3，然后1线程醒了，睡够3秒了，打印了2
+         */
+        Thread thread = new Thread(() -> {
+            System.out.println(1);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(2);
+        });
+        thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(3);
+        thread.interrupt();
     }
 
     private static void ThreadState() {
